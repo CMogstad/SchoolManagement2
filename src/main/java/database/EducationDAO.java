5d098e849b75cb5c1ee38220640ac2337469206a
GustavAlbrechtGitHub <93325097+GustavAlbrechtGitHub@users.noreply.github.com>
2021-12-14 15:39:41 +0100

    Education dao impl part

--- src/main/java/database/EducationDAO.java
+++ src/main/java/database/EducationDAO.java
@@ -1,17 +1,30 @@
 package database;
 
+import entities.Course;
 import entities.Education;
+import entities.Student;
+
+import java.util.List;
 
 public interface EducationDAO {
 
     void addEducation(Education education);
 
-    /*
-      Add educations of different kind
-• Update education information
-• Show information about a specific education
-• Show all educations
-• Remove an education
+    void updateEducation(Education education);
 
-     */
+    void removeEducation(int id);
+
+    Education findEducation(int id);
+
+    List<Education> findEducationByName(String name);
+
+    List<Education> showAllEducations();
+
+    List<Course> listEducationCourses(int id);
+
+    List<Education> showAllEducationsWithoutCourse();
+
+    List<Student> listEducationStudents(int id);
+
+    List<Education> listEducationWithoutStudents();
 }
