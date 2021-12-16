0b9852219c6f16663496cc6679a2844c331878d0
strawberryPenny <93320042+strawberryPenny@users.noreply.github.com>
2021-12-16 15:07:10 +0100

    Updates to Consoles UI for student and statistics

--- src/main/java/userinterface/StatisticsConsoleUI.java
+++ src/main/java/userinterface/StatisticsConsoleUI.java
@@ -1,15 +1,122 @@
 package userinterface;
 
 import controller.StatisticsController;
-import database.StatisticsDao;
+import entities.Course;
+import entities.Education;
+import entities.Student;
+import entities.Teacher;
+
+import java.util.List;
 
 public class StatisticsConsoleUI {
 
     StatisticsController stc;
-    ConsoleInput uInput;
+    MainConsoleUI mainUI;
+    ConsoleInput input;
 
-    public StatisticsConsoleUI(StatisticsController stc, ConsoleInput uInput) {
+    public StatisticsConsoleUI(StatisticsController stc, MainConsoleUI mainUI, ConsoleInput input) {
         this.stc = stc;
-        this.uInput = uInput;
+        this.mainUI = mainUI;
+        this.input = input;
+    }
+
+    public void printStatisticsMenu() {
+        System.out.println("""
+                                
+                _______________________________________________
+                                School Management
+                                 Statistics Menu
+                _______________________________________________
+                1. Show Students by Date Of Birth
+                2. Show Course with highest points
+                3. Show Course with lowest points
+                4. Show Education with most students
+                5. Show Teacher with most Courses
+                6. Show Education with most Courses
+                7. Show average of Employment year
+                8. List Employment year Ascending Order
+                9. Show number of students by Education
+                10. Show numbers of courses by Education
+                0. Back to Main Menu
+
+                Please choose: """);
+    }
+
+    public void showStudentsByDateOfBirthUI() {
+        System.out.print("Please enter earliest year: ");
+        int eYear = input.readInt();
+
+        System.out.print("Please enter latest year: ");
+        int lYear = input.readInt();
+
+        List<Student> list = stc.studentsByDateOfBirthController(eYear, lYear);
+
+        mainUI.printList(list);
+    }
+
+    public void courseHighestPointUI() {
+
+        List<Course> list = stc.courseHighestPointsController();
+
+        mainUI.printList(list);
+    }
+
+    public void courseLowestPointUI() {
+
+        List<Course> list = stc.courseLowestPointsController();
+
+        mainUI.printList(list);
+    }
+
+    public void educationMostStudentsUI() {
+
+        List<Education> list = stc.educationMostStudentsController();
+
+        mainUI.printList(list);
+    }
+
+    public void teacherMostCoursesUI(){
+
+        List<Teacher> list  = stc.teacherMostCoursesController();
+
+        mainUI.printList(list);
+
+    }
+    public void educationMostCoursesUI(){
+
+        List<Education> list = stc.educationMostCoursesController();
+
+        mainUI.printList(list);
+
+    }
+    public void averageEmploymentYearUI(){
+
+        double avg = stc.averageEmploymentYearController();
+        System.out.println("Average employment year: " + avg);
+
+    }
+
+    public void numberOfStudentsByEducationUI(){
+
+        List<Education> list = stc.getListOfAllEducationsController();
+
+        for (Education e: list) {
+            System.out.println(e.getName() + " " + e.getStudents().size());
+        }
+
+    }
+    public void numberOfCoursesByEducationUI(){
+
+        List<Education> list = stc.getListOfAllEducationsController();
+
+        for (Education e : list) {
+
+            System.out.println(e.getName() + " " + e.getCourses().size());
+        }
+
+    }
+    public void listEmploymentYearAscOrdUI(){
+        List<Teacher> list = stc.listEmploymentYearAscOrdController();
+        mainUI.printList(list);
     }
 }
--- src/main/java/userinterface/StudentConsoleUI.java
+++ src/main/java/userinterface/StudentConsoleUI.java
@@ -1,14 +1,146 @@
 package userinterface;
 
 import controller.StudentController;
+import entities.Student;
+
+import java.util.List;
 
 public class StudentConsoleUI {
 
     StudentController sc;
-    ConsoleInput uInput;
+    MainConsoleUI mUI;
+    ConsoleInput input;
 
-    public StudentConsoleUI(StudentController sc, ConsoleInput uInput) {
+    public StudentConsoleUI(StudentController sc, MainConsoleUI mUI, ConsoleInput uInput) {
         this.sc = sc;
-        this.uInput = uInput;
+        this.mUI = mUI;
+        this.input = uInput;
+    }
+
+    public void printStudentMenu() {
+        System.out.print("""
+                                
+                _______________________________________________
+                                School Management
+                                    Student Menu
+                _______________________________________________
+                1. Add Student
+                2. Update First Name
+                3. Update Last Name
+                4. Update Date of Birth
+                5. Add Student To Education
+                6. Remove Student From Education
+                7. Remove Student
+                8. Find Student By Last Name
+                9. Show All Students
+                10. Show All Students Without Education
+                0. Back to Main Menu
+
+                Please choose: """);
+    }
+
+    public void readStudentUI() {
+        System.out.print("\nPlease enter first name: ");
+        String firstName = input.readString();
+
+        System.out.print("Please enter last name: ");
+        String lastName = input.readString();
+
+        System.out.print("Please enter date of birth: ");
+        String dateOfBirth = input.readString();
+
+        sc.createStudentController(firstName, lastName, dateOfBirth);
+
+        System.out.println("\nStudent has been added!");
+    }
+
+    public void updateStudentFirstNameUI() {
+        System.out.print("\nPlease enter ID of student: ");
+        int studentId = input.readInt();
+
+        System.out.print("Please enter new first name of student: ");
+        String newName = input.readString();
+
+        sc.updateStudentFirstNameController(studentId, newName);
+
+        System.out.println("\nStudent has been updated!");
+    }
+
+    public void updateStudentLastNameUI() {
+        System.out.print("\nPlease enter ID of student: ");
+        int studentId = input.readInt();
+
+        System.out.print("Please enter new last name of student: ");
+        String newName = input.readString();
+
+        sc.updateStudentLastNameController(studentId, newName);
+
+        System.out.println("\nStudent has been updated!");
+    }
+
+    public void updateStudentDateOfBirthUI() {
+        System.out.print("\nPlease enter ID of student: ");
+        int studentId = input.readInt();
+
+        System.out.print("Please enter new date of birth of student: ");
+        String newDateOfBirth = input.readString();
+
+        sc.updateStudentDateOfBirthController(studentId, newDateOfBirth);
+
+        System.out.println("\nStudent has been updated!");
+    }
+
+    public void removeStudentUI() {
+        System.out.print("\nPlease enter ID of student: ");
+        int studentId = input.readInt();
+
+        sc.deleteStudentByID(studentId);
+
+        System.out.println("\nStudent has been removed!");
+    }
+
+    public void findStudentByLastNameUI() {
+        System.out.print("\nPlease enter last name: ");
+        String lastName = input.readString();
+
+        List<Student> students = sc.findStudentByLastNameController(lastName);
+
+        mUI.printList(students);
+    }
+
+    public void showAllStudentsUI() {
+        List<Student> students = sc.showAllStudentsController();
+
+        mUI.printList(students);
+    }
+
+    public void showAllStudentsWithoutEducationUI() {
+        List<Student> students = sc.showAllStudentsWithoutEducationController();
+
+        mUI.printList(students);
+    }
+
+    public void addEducationToStudentUI() {
+        System.out.print("\nPlease enter ID of education: ");
+        int eID = input.readInt();
+
+        System.out.print("Please enter ID of student: ");
+        int sID = input.readInt();
+
+        sc.addEducationToStudentController(eID, sID);
+
+        System.out.println("Education and student has been joined! ");
+    }
+
+    public void removeEducationFromStudentUI() {
+        System.out.print("\nPlease enter ID of education you want to remove: ");
+        int eID = input.readInt();
+
+        System.out.print("Please enter ID of student: ");
+        int sID = input.readInt();
+
+        sc.removeStudentFromEducationController(eID, sID);
+
+        System.out.println("\nEducation has been removed from student!");
     }
 }
